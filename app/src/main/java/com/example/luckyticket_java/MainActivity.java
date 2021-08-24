3b4ac9cce8758cede03a29ccbaffa35a63f5f6a1
Sasha <sashapysmenny@gmail.com>
2021-08-24 13:16:28 +0300

    refactoring

--- app/src/main/java/com/example/luckyticket_java/MainActivity.java
+++ app/src/main/java/com/example/luckyticket_java/MainActivity.java
@@ -16,51 +16,42 @@ import java.util.Timer;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int MAX_NUMBER_OF_DIGITS = 6;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    public void check_onClick(View view){
-        EditText text = findViewById(R.id.digits);
+    public void checkTicket(View view){
+        EditText text = findViewById(R.id.etTicketNumber);
         String ticket_number = text.getText().toString();
-        ImageView bulb = findViewById(R.id.bulb);
+        ImageView bulb = findViewById(R.id.imgBulb);
 
         Drawable green = ContextCompat.getDrawable(this, getResources()
                 .getIdentifier("@drawable/ic_green_bulb", null, getPackageName()));
 
         Drawable red = ContextCompat.getDrawable(this, getResources()
                 .getIdentifier("@drawable/ic_red_bulb", null, getPackageName()));
+        
 
-        int left_sum = 0;
-        int  right_sum = 0;
+        if (ticket_number.length() == MAX_NUMBER_OF_DIGITS) {
+            int left_sum = ticket_number.substring(0,3).chars().sum();
+            int right_sum = ticket_number.substring(3,6).chars().sum();
 
-        if (ticket_number.length() == 6) {
-
-            for(int i = 0; i < ticket_number.length(); i++){
-                if (i < 3){
-                    left_sum += ticket_number.charAt(i);
-                }
-                else{
-                    right_sum += ticket_number.charAt(i);
-                }
-            }
             if (left_sum == right_sum){
                 bulb.setImageDrawable(green);
                 Toast.makeText(this, "Lucky", Toast.LENGTH_LONG).show();
             }
-
             else{
                 bulb.setImageDrawable(red);
                 Toast.makeText(this, "Unlucky", Toast.LENGTH_LONG).show();
             }
-            // Возвращает лампочку в исходное состояние через 3 секунды
+
             backToDefault(bulb);
 
         } else {
-            Toast.makeText(this, "Something went wrong. Try again!", Toast.LENGTH_LONG).show();
+            Toast.makeText(this, "Too short. Enter 6 digits", Toast.LENGTH_LONG).show();
         }
     }
 
